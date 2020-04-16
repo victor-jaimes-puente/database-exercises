18ca30b2fd8cb6ab8a71d1d5d0cd558ca022c264
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-16 15:13:43 -0500

    beganOrderByEx

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,15 @@
 USE codeup_test_db;
+select name from albums where artist = 'PinK Floyd';
+select name from albums where release_date<1980;
+select name from albums where artist = 'Michael Jackson';
+
+UPDATE albums
+SET  sales = sales * 10;
+
+update albums
+set release_date = 1800
+where release_date<1980;
+
+update albums
+set artist = 'Peter Jackson'
+where artist = 'Michael Jackson';
---
+++ where_exercises.sql
@@ -0,0 +1,38 @@
+
+
+
+select first_name
+from employees
+where first_name  like 'Irena'
+OR first_name like 'Vidya'
+OR first_name like 'Maya'
+AND gender = 'M'
+
+order by first_name;
+
+select first_name
+from employees
+where first_name like 'e%'
+order by first_name;
+
+select hire_date
+from employees
+where hire_date like '199%'
+order by hire_date;
+
+select first_name
+from employees
+where birth_date like '%%%%-12-25'
+order by hire_date;
+
+select first_name
+from employees
+where first_name like '%q%'
+order by hire_date;
+
+select first_name
+from employees
+where first_name like 'e%'
+order by emp_no;
+
+
