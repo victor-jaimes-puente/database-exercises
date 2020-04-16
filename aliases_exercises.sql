d59b9acbac305c462a46a9e42d58b18ce950381a
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-16 16:51:01 -0500

    updatingTreeB4Break

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -2,3 +2,8 @@ use employees;
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
 LIMIT 10;
+
+SELECT DISTINCT last_name
+FROM employees
+order by last_name desc
+limit 10;
--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,19 +1,25 @@
 
 
-
+use employees;
 
 select first_name, last_name
 from employees
 where first_name  like 'Irena'
-OR first_name like 'Vidya'
-OR first_name like 'Maya'
-AND gender = 'M'
+   OR first_name like 'Vidya'
+   OR first_name like 'Maya'
+    AND gender = 'M'
 order by last_name, first_name;
 
 select first_name, last_name
 from employees
 where first_name like '%e%'
 order by  emp_no desc;
+
+select emp_no, concat(first_name, ' ', last_name) as full_name
+from employees
+where first_name like 'e%'
+and first_name like '%e'
+order by  emp_no ;
 
 select hire_date
 from employees
@@ -30,13 +36,17 @@ from employees
 where first_name like '%q%'
 order by hire_date;
 
-select first_name
-from employees
-where first_name like 'e%'
-order by emp_no;
+
 
 select first_name, birth_date, hire_date
 from employees
 where birth_date like '%%%%-12-25'
-and  hire_date like '%%%%-12-25'
+  and  hire_date like '%%%%-12-25'
 limit 5;
+
+select hire_date, birth_date, concat(first_name,' ', last_name) as full_name,
+    DATEDIFF(CURDATE(),hire_date) as days_employed
+from employees
+where hire_date like '199%'
+and birth_date like '%%%%-12-25'
+order by days_employed desc;
