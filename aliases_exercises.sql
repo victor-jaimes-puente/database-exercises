4e29165c411607e233f24ed9497bf3e4f88d2973
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-17 10:07:05 -0500

    finishedAliasesEx

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,6 +1,8 @@
 use employees;
-SELECT CONCAT(first_name, ' ', last_name) AS full_name
+SELECT CONCAT(emp_no, ' ', first_name, ' ', last_name) AS full_name,
+       birth_date as DOB
 FROM employees
+order by emp_no
 LIMIT 10;
 
 SELECT DISTINCT last_name
