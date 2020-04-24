518e15ee1493cf8095310570320b770c3514627f
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-23 20:31:44 -0500

    updating

---
+++ join_exercises.sql
@@ -0,0 +1,35 @@
+use employees;
+
+# show tables
+# describe roles
+CREATE TABLE users (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       role_id INT UNSIGNED DEFAULT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+show tables;
+
+INSERT INTO roles (name) VALUES ('Alpha1');
+INSERT INTO roles (name) VALUES ('Bravo1');
+INSERT INTO roles (name) VALUES ('Charlie1');
+INSERT INTO roles (name) VALUES ('Delta1');
+
+
+INSERT INTO users (name, email, role_id) VALUES
+('bob', 'bob@example.com', 1),
+('joe', 'joe@example.com', 2),
+('sally', 'sally@example.com', 3),
+('adam', 'adam@example.com', 3),
+('jane', 'jane@example.com', null),
+('mike', 'mike@example.com', null);
+
+# INSERT INTO users (name, email, role_id) VALUES
+# ('bob', 'bob@example.com', 1),
+# ('joe', 'joe@example.com', 2),
+# ('sally', 'sally@example.com', 3),
+# ('adam', 'adam@example.com', 3),
+# ('jane', 'jane@example.com', null),
+# ('mike', 'mike@example.com', null);
